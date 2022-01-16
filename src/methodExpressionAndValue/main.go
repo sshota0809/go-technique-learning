e72c3fe720ab687ac39b5f9783594b62019b7197
sshota0809 <8736380+sshota0809@users.noreply.github.com>
2022-01-16 12:21:57 +0900

    Change to pointer receiver

--- src/methodExpressionAndValue/main.go
+++ src/methodExpressionAndValue/main.go
@@ -6,17 +6,21 @@ type Number struct {
 	n int
 }
 
-func (n Number) Add(i int) int {
-	return n.n + i
+func (n *Number) Add(i int) int {
+	n.n = n.n + i
+	return n.n
 }
 
 func main() {
 	// メソッド式の第一引数はレシーバになる
-	add := Number.Add
+	add := (*Number).Add
+	n := &Number{1}
 	fmt.Printf("%T\n", add)
-	fmt.Println(add(Number{1}, 2))
+	fmt.Println(add(n, 2))
+	fmt.Println(add(n, 2))
 
-	increment := Number{1}.Add
+	increment := (&Number{1}).Add
 	fmt.Printf("%T\n", increment)
+	fmt.Println(increment(2))
 	fmt.Println(increment(2))
 }
